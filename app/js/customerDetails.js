878fbfe46265ae8ac1493a0cb0873d1ef28c4652
Avirup Nandi <avirup.nandi@tcs.com>
2017-07-24 09:16:49 +0300

    Added left navigation

--- app/js/customerDetails.js
+++ app/js/customerDetails.js
@@ -1,9 +1,14 @@
 import $ from "jquery"
 import Template from "hb-template"
 
+const renderNavigation = (customerDetails) => {
+  const navigation = Template.Navigation(customerDetails)
+  $('#customer-details').append(navigation)
+}
+
 const renderCustomerDetails = (container, customerDetails) => {
-  const customerDetailsView = Template.CustomerDetails(customerDetails)
-  $('#search-result').after(customerDetailsView)
+  $('#search-result').after('<div id="customer-details"></div>')
+  renderNavigation(customerDetails)
 }
 
 export {renderCustomerDetails}
